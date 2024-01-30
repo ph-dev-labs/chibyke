e5147dc060ed55312f2e4c9114e154509588ab31
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2024-01-30 10:55:00 +0000

    adjust ment

--- bnclimited.net/script.js
+++ bnclimited.net/script.js
@@ -1,54 +1,71 @@
-// Fetch data from the API
-function fetchData() {
+let user;
+
+async function fetchUser() {
+  try {
+    const response = await fetch("https://testapi.devtoolsdaily.com/users");
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok, status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    // Ensure there is at least one user in the data array
+    if (data.length > 0) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      user = data[randomIndex];
+    } else {
+      throw new Error("No user data available");
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+  }
+}
+
+async function fetchData() {
+  try {
+    await fetchUser();
     const randomNumber = Math.floor(Math.random() * 10) + 1;
-    fetch('https://blockchain.info/unconfirmed-transactions?format=json')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        // Assuming you want to check the first transaction in the response
-        const firstTransaction = data.txs[randomNumber];
-  
-        // Replace 'TARGET_AMOUNT' with the specific amount you are interested in (in BTC)
-        const targetAmount = 0.01;
-  
-        // Check each output in the transaction
-        firstTransaction.out.forEach(output => {
-          const address = output.addr;
-          const amount = output.value / 1e8;
-          const newDate = new Date();
-          const time = newDate.toTimeString()
-  
-          // Check if the address received the specified amount
-          if (amount >= targetAmount) {
-            // Display the toaster notification
-            showToast(`BTC Received!\n${amount} BTC at ${address} Time: ${time}`);
-          }
-        });
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
+    const response = await fetch('https://blockchain.info/unconfirmed-transactions?format=json');
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const firstTransaction = data.txs[randomNumber];
+    const targetAmount = 0.01;
+
+    firstTransaction.out.forEach(output => {
+      const address = output.addr;
+      const amount = output.value / 1e8;
+      const newDate = new Date();
+      const time = newDate.toTimeString();
+      const name = user.firstName;
+
+      if (amount >= targetAmount) {
+        showToast(`${name} just received\n${amount} \n BTC at ${address} \n Time: ${time}`);
+      }
+    });
+  } catch (error) {
+    console.error('Fetch error:', error);
   }
-  
-  // Function to display the toaster notification
-  function showToast(message) {
-    Toastify({
-      text: message,
-      duration: 5000, // Duration in milliseconds
-      gravity: 'bottom', // Position the toaster at the bottom
-      position: 'right', // Center the toaster horizontally
-      outerWidth: "auto",
-      style: {
-        width: "auto"
-      },
-      background: 'linear-gradient(to right, #00b09b, #96c93d)', // Customize the background color
-    }).showToast();
-  }
-  
-  // Fetch data every 60 seconds (adjust as needed)
-  setInterval(fetchData, 6000);
-  
+}
+
+function showToast(message) {
+  Toastify({
+    text: message,
+    duration: 5000,
+    gravity: 'bottom',
+    position: 'right',
+    outerWidth: "auto",
+    style: {
+      width: "auto", // Adjust width as needed
+      overflow: "hidden",
+    },
+    background: 'linear-gradient(to right, #00b09b, #96c93d)',
+  }).showToast();
+}
+
+// Fetch data every 60 seconds
+setInterval(fetchData, 6000);
